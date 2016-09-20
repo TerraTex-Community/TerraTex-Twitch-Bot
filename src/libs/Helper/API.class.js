df1b917304eb23fbd5b35a1bba68bdce03bce6b2
Colin Weber <colin.weber@sap.com>
2016-09-20 13:56:03 +0200

    remove old code

--- src/libs/Helper/API.class.js
+++ src/libs/Helper/API.class.js
@@ -15,21 +15,6 @@ class APIHelper {
                 url: "https://tmi.twitch.tv/group/user/" + channel + "/chatters"
             }, function (err, res, body) {
                 if (!err) {
-                    // console.log(typeof body);
-                    // if (body.includes("<html>")) {
-                    //     callback(body, null);
-                    //     return;
-                    // }
-                    //
-                    // let users = [];
-                    // let chatter = JSON.parse(body).chatters;
-                    // if (chatter) {
-                    //     users = users.concat(chatter.moderators).concat(chatter.staff).concat(chatter.admins).concat(chatter.global_mods);
-                    //     users = users.concat(chatter.viewers);
-                    // }
-                    //
-                    // callback(null, users);
-
                     let users = [];
                     let chatter = body.chatters;
                     if (chatter) {
